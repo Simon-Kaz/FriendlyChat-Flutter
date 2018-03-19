74bd8fad2e4f6d1178b23f286f3ace258cbc61b6
Szymon Kazmierczak <szymonkaz@gmail.com>
2018-03-19 23:32:42 +0000

    Added some firebase goodness, storing message directly in firebaseDB, ability to upload images, replaced listview with FirebaseAnimatedList

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'dart:async';
+import 'package:firebase_analytics/firebase_analytics.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:firebase_database/ui/firebase_animated_list.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:image_picker/image_picker.dart';
+import 'dart:math';
+import 'dart:io';
 
-const String _name = "Your Name";
-
+// Themes for iOS and Android
 final ThemeData kIOSTheme = new ThemeData(
     primarySwatch: Colors.orange,
     primaryColor: Colors.grey[100],
@@ -15,7 +24,30 @@ final ThemeData kDefaultTheme = new ThemeData(
     accentColor: Colors.orangeAccent[400]
 );
 
+// Global instances for Firebase/Google Sign In
+final googleSignIn = new GoogleSignIn();
+final analytics = new FirebaseAnalytics();
+final auth = FirebaseAuth.instance;
+final reference = FirebaseDatabase.instance.reference().child('messages');
+
+
 void main() => runApp(new FriendlyChatApp());
+
+Future<Null> _ensureLoggedIn() async {
+  GoogleSignInAccount user = googleSignIn.currentUser;
+  if (user == null)
+    user = await googleSignIn.signInSilently();
+  if (user == null) {
+    await googleSignIn.signIn();
+    analytics.logLogin();
+  }
+  if (await auth.currentUser() == null) {
+    GoogleSignInAuthentication credentials =
+    await googleSignIn.currentUser.authentication;
+    await auth.signInWithGoogle(
+        idToken: credentials.idToken, accessToken: credentials.accessToken);
+  }
+}
 
 class FriendlyChatApp extends StatelessWidget {
   @override
@@ -35,9 +67,8 @@ class ChatScreen extends StatefulWidget {
   State createState() => new ChatScreenState();
 }
 
-class ChatScreenState extends State<ChatScreen> with TickerProviderStateMixin {
+class ChatScreenState extends State<ChatScreen> {
   final TextEditingController _textController = new TextEditingController();
-  final List<ChatMessage> _messages = <ChatMessage>[];
   bool _isComposing = false;
 
   @override
@@ -52,13 +83,18 @@ class ChatScreenState extends State<ChatScreen> with TickerProviderStateMixin {
       body: new Container(
           child: new Column(
             children: <Widget>[
-              new Flexible(
-                child: new ListView.builder(
-                    padding: new EdgeInsets.all(8.0),
-                    reverse: true,
-                    itemBuilder: (_, int index) => _messages[index],
-                    itemCount: _messages.length
-                ),
+              new Flexible(child: new FirebaseAnimatedList(
+                  query: reference,
+                  sort: (a, b) => b.key.compareTo(a.key),
+                  padding: new EdgeInsets.all(8.0),
+                  reverse: true,
+                  itemBuilder: (_, DataSnapshot snapshot,
+                      Animation<double> animation, index) {
+                    return new ChatMessage(
+                        snapshot: snapshot,
+                        animation: animation
+                    );
+                  })
               ),
               new Divider(height: 1.0),
               new Container(
@@ -91,6 +127,20 @@ class ChatScreenState extends State<ChatScreen> with TickerProviderStateMixin {
         margin: const EdgeInsets.symmetric(horizontal: 8.0),
         child: new Row(
             children: <Widget>[
+              new Container(
+                margin: new EdgeInsets.symmetric(horizontal: 4.0),
+                child: new IconButton(
+                    icon: new Icon(Icons.photo_camera), onPressed: () async {
+                  await _ensureLoggedIn();
+                  File imageFile = await ImagePicker.pickImage();
+                  int random = new Random().nextInt(100000);
+                  StorageReference ref =
+                  FirebaseStorage.instance.ref().child("image_$random.jpg");
+                  StorageUploadTask uploadTask = ref.put(imageFile);
+                  Uri downloadUrl = (await uploadTask.future).downloadUrl;
+                  _sendMessage(imageUrl: downloadUrl.toString());
+                }),
+              ),
               new Flexible(
                 child: new TextField(
                   controller: _textController,
@@ -127,43 +177,37 @@ class ChatScreenState extends State<ChatScreen> with TickerProviderStateMixin {
     );
   }
 
-  void _handleSubmitted(String text) {
+  void _handleSubmitted(String text) async {
     _textController.clear();
     setState(() {
       _isComposing = false;
     });
-    ChatMessage message = new ChatMessage(
-      text: text,
-      animationController: new AnimationController(
-          duration: new Duration(milliseconds: 500),
-          vsync: this),
-    );
-    setState(() {
-      _messages.insert(0, message);
-    });
-    message.animationController.forward();
+    await _ensureLoggedIn();
+    _sendMessage(text: text);
   }
 
-  @override
-  void dispose() {
-    for (ChatMessage message in _messages) {
-      message.animationController.dispose();
-    }
-    super.dispose();
+  void _sendMessage({String text, String imageUrl}) {
+    reference.push().set({
+      'text': text,
+      'imageUrl': imageUrl,
+      'senderName': googleSignIn.currentUser.displayName,
+      'senderPhotoUrl': googleSignIn.currentUser.photoUrl,
+    });
+    analytics.logEvent(name: 'send_message');
   }
 }
 
 class ChatMessage extends StatelessWidget {
-  ChatMessage({this.text, this.animationController});
+  ChatMessage({this.snapshot, this.animation});
 
-  final String text;
-  final AnimationController animationController;
+  final DataSnapshot snapshot;
+  final Animation animation;
 
   @override
   Widget build(BuildContext context) {
     return new SizeTransition(
         sizeFactor: new CurvedAnimation(
-            parent: animationController, curve: Curves.fastOutSlowIn),
+            parent: animation, curve: Curves.fastOutSlowIn),
         axisAlignment: 0.0,
         child: new Container(
           margin: const EdgeInsets.symmetric(vertical: 10.0),
@@ -172,19 +216,29 @@ class ChatMessage extends StatelessWidget {
             children: <Widget>[
               new Container(
                 margin: const EdgeInsets.only(right: 16.0),
-                child: new CircleAvatar(child: new Text(_name[0])),
+                child: new CircleAvatar(
+                    child: new CircleAvatar(
+                        backgroundImage: new NetworkImage(
+                            snapshot.value['senderPhotoUrl']))
+                ),
               ),
               new Expanded(
                 child: new Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: <Widget>[
-                    new Text(_name, style: Theme
-                        .of(context)
-                        .textTheme
-                        .subhead),
+                    new Text(snapshot.value['senderName'],
+                        style: Theme
+                            .of(context)
+                            .textTheme
+                            .subhead),
                     new Container(
                       margin: const EdgeInsets.only(top: 5.0),
-                      child: new Text(text),
+                      child: snapshot.value['imageUrl'] != null ?
+                      new Image.network(
+                        snapshot.value['imageUrl'],
+                        width: 250.0,
+                      ) :
+                      new Text(snapshot.value['text']),
                     ),
                   ],
                 ),
